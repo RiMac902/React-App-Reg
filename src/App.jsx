2574c68ad747ce707fc06586cb2439163ca23d59
RiMac902 <romansk902@gmail.com>
2022-09-08 01:13:13 +0300

    HomePage come back)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProjectPage from "./pages/ProjectsPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PrivateRoute from "./components/PrivateRoute";
+import HomePage from "./pages/HomePage";
 import TasksPage from "./pages/TasksPage";
 
 
@@ -12,6 +13,7 @@ import TasksPage from "./pages/TasksPage";
 const App = () => {
     return (
         <Routes>
+            <Route path={'/'} element={<HomePage/>}/>
             <Route path={'/projects'} element={<PrivateRoute> <ProjectPage/> </PrivateRoute>}/>
             <Route path={'/login'} element={<LoginPage/>}/>
             <Route path={'/projects/tasks'} element={<TasksPage/>}/>
